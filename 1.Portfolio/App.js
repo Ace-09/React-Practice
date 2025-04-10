f9cb862248e4a231912a9fefb8055732fb8e4941
Nimish Lohumi <nimishlohumi@Nimishs-MacBook-Air.local>
2025-04-11 01:04:22 +0530

    0.01 10.4.25

--- 1.Portfolio/App.js
+++ 1.Portfolio/App.js
@@ -2,16 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import About from "./src/components/About";
+import ErrorPage from "./src/common/ErrorPage";
 
 const AppLayout = () => {
-    return(
-        <div className="app">
-            <Header />
-            <Body />
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
-
+root.render(<RouterProvider router={router} />);
